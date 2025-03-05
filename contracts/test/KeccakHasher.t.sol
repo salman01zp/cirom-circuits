0c0b8ebff5565702d62c962c464f7a32c91d926d
salman01zp <pathansalman555@gmail.com>
2025-03-05 22:58:21 +0530

    forge fmt

--- contracts/test/KeccakHasher.t.sol
+++ contracts/test/KeccakHasher.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import { KeccakHasher } from "../src/hasher/KeccakHasher.sol";
+import {KeccakHasher} from "../src/hasher/KeccakHasher.sol";
 import "../src/interface/IHasher.sol";
 
 contract KeccakHasherTest is Test {
@@ -10,10 +10,10 @@ contract KeccakHasherTest is Test {
 
     // Set up function to deploy the KeccakHasher contract before each test
     function setUp() public {
-        hasher = new KeccakHasher(); 
+        hasher = new KeccakHasher();
     }
 
-    function test_hashLeftRight() public view { 
+    function test_hashLeftRight() public view {
         bytes32 left = keccak256(abi.encodePacked("left"));
         bytes32 right = keccak256(abi.encodePacked("right"));
         bytes32 result = hasher.hashLeftRight(left, right);
@@ -21,7 +21,5 @@ contract KeccakHasherTest is Test {
         // Calculate the expected hash manually (keccak256 of the concatenated inputs)
         bytes32 expected = keccak256(abi.encodePacked(left, right));
         assertEq(result, expected, "The hashLeftRight result is incorrect");
-
     }
-
 }
