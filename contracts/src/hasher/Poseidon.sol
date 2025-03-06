30c5ffc3756ca2252d52f56db8e2f2502542c884
salman01zp <pathansalman555@gmail.com>
2025-03-06 15:24:11 +0530

    Add poseidon hasher contract

---
+++ contracts/src/hasher/Poseidon.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+/// @title Poseidon hash functions for 2 input elements.
+
+library PoseidonT3 {
+    function poseidon(uint256[2] memory input) public pure returns (uint256) {}
+}
